ea3fd879c67f1849ed936a4c7074d3c178343849
aliahadie <ali.ahadi.official@gmail.com>
2024-12-11 02:07:35 +0330

    adding dynamic urls for base.html

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path,include
 from blog.views import *
 
+app_name='blog'
+
 urlpatterns = [
     path('',blog_home,name='blog'),
     path('blog-single/',blog_single,name='blog_single')
--- website/urls.py
+++ website/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path,include
 from website.views import *
 
+app_name='website'
+
 urlpatterns = [
     path('',index_view,name='index'),
     path('about/',about_view,name='about'),
