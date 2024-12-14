24177e0d49bfd870f9c55a3d2a1478681873b502
aliahadie <ali.ahadi.official@gmail.com>
2024-12-15 01:23:38 +0330

    create models for website and blog app - register and edit in admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from blog.models import Post
+
 
 # Register your models here.
+class BlogAdmin(admin.ModelAdmin):
+    date_hierarchy='created_date'
+    list_display = ('title','status','created_date')
+    search_fields=('title','content')
+    
+    
+admin.site.register(Post,BlogAdmin)    
--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from website.models import Contact
 
 # Register your models here.
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display=('name','subject','created_date')
+    date_hierarchy = 'created_date'
+
+
+admin.site.register(Contact,ContactAdmin)
